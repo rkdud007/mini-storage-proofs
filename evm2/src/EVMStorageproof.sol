58d4fe79ba5ee4bcbd6fbb5e8a4514936beb57c1
Pia <gayeongparkk@gmail.com>
2023-12-03 19:21:29 +0900

    evm2

--- evm2/src/EVMStorageproof.sol
+++ evm2/src/EVMStorageproof.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.19;
+pragma solidity ^0.8.13;
 
 import "../lib/Solidity-RLP/contracts/RLPReader.sol";
 import "./lib/external/trie/Lib_SecureMerkleTrie.sol";
