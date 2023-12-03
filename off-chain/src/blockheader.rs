3d007abc03c4d06fc8fbe2b81a39c41296c0678b
Pia <gayeongparkk@gmail.com>
2023-12-03 15:45:47 +0900

    Access block header

---
+++ off-chain/src/blockheader.rs
@@ -0,0 +1,126 @@
+use ethers::utils::hex;
+use rlp::{Encodable, RlpStream};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone)]
+pub struct EvmBlockHeader {
+    pub parent_hash: String,
+    pub uncle_hash: String,
+    pub coinbase: String,
+    pub state_root: String,
+    pub transactions_root: String,
+    pub receipts_root: String,
+    pub logs_bloom: String,
+    pub difficulty: u64,
+    pub number: u64,
+    pub gas_limit: u64,
+    pub gas_used: u64,
+    pub timestamp: u64,
+    pub extra_data: String,
+    pub mix_hash: String,
+    pub nonce: String,
+    pub base_fee_per_gas: Option<u64>,
+    pub withdrawals_root: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct EvmBlockHeaderFromRpc {
+    pub number: String,
+    pub hash: String,
+    pub difficulty: String,
+    pub extra_data: String,
+    pub gas_limit: String,
+    pub gas_used: String,
+    pub logs_bloom: String,
+    pub miner: String,
+    pub mix_hash: String,
+    pub nonce: String,
+    pub parent_hash: String,
+    pub receipts_root: String,
+    pub sha3_uncles: String,
+    pub size: String,
+    pub state_root: String,
+    pub timestamp: String,
+    pub total_difficulty: String,
+    pub transactions_root: String,
+    pub base_fee_per_gas: Option<String>,
+    pub withdrawals_root: Option<String>,
+}
+
+impl From<&EvmBlockHeaderFromRpc> for EvmBlockHeader {
+    fn from(value: &EvmBlockHeaderFromRpc) -> Self {
+        Self {
+            parent_hash: value.parent_hash.clone(),
+            uncle_hash: value.sha3_uncles.clone(),
+            coinbase: value.miner.clone(),
+            state_root: value.state_root.clone(),
+            transactions_root: value.transactions_root.clone(),
+            receipts_root: value.receipts_root.clone(),
+            logs_bloom: value.logs_bloom.clone(),
+            difficulty: value.difficulty.clone().parse::<u64>().unwrap(),
+            number: value.number.clone().parse::<u64>().unwrap(),
+            gas_limit: value.gas_limit.clone().parse::<u64>().unwrap(),
+            gas_used: value.gas_used.clone().parse::<u64>().unwrap(),
+            timestamp: value.timestamp.clone().parse::<u64>().unwrap(),
+            extra_data: value.extra_data.clone(),
+            mix_hash: value.mix_hash.clone(),
+            nonce: value.nonce.clone(),
+            base_fee_per_gas: value
+                .clone()
+                .base_fee_per_gas
+                .and_then(|x| Some(x.parse::<u64>().unwrap())),
+            withdrawals_root: value.withdrawals_root.clone(),
+        }
+    }
+}
+
+impl Encodable for EvmBlockHeader {
+    fn rlp_append(&self, s: &mut RlpStream) {
+        s.begin_list(
+            15 + self.base_fee_per_gas.is_some() as usize
+                + self.withdrawals_root.is_some() as usize,
+        );
+
+        s.append(&safe_hex_decode(&self.parent_hash));
+        s.append(&safe_hex_decode(&self.uncle_hash));
+        s.append(&safe_hex_decode(&self.coinbase));
+        s.append(&safe_hex_decode(&self.state_root));
+        s.append(&safe_hex_decode(&self.transactions_root));
+        s.append(&safe_hex_decode(&self.receipts_root));
+        s.append(&safe_hex_decode(&self.logs_bloom));
+
+        // Numeric fields can be appended directly if they are already u64
+        s.append(&self.difficulty);
+        s.append(&self.number);
+        s.append(&self.gas_limit);
+        s.append(&self.gas_used);
+        s.append(&self.timestamp);
+
+        s.append(&safe_hex_decode(&self.extra_data));
+        s.append(&safe_hex_decode(&self.mix_hash));
+        s.append(&safe_hex_decode(&self.nonce));
+
+        if let Some(base_fee) = self.base_fee_per_gas {
+            s.append(&base_fee);
+        }
+
+        if let Some(ref withdrawals_root) = self.withdrawals_root {
+            s.append(&safe_hex_decode(withdrawals_root));
+        }
+    }
+}
+
+fn safe_hex_decode(s: &str) -> Vec<u8> {
+    // Ensure the string is without the '0x' prefix
+    let s = if s.starts_with("0x") { &s[2..] } else { s };
+
+    // Pad the string with a leading zero if it has an odd length
+    let s = if s.len() % 2 != 0 {
+        format!("0{}", s)
+    } else {
+        s.to_string()
+    };
+
+    hex::decode(&s).unwrap()
+}
---
+++ off-chain/src/main.rs
@@ -0,0 +1,73 @@
+use std::sync::Arc;
+
+use anyhow::Result;
+use ethers::{prelude::*, utils::keccak256};
+use rlp::{Encodable, RlpStream};
+
+use crate::blockheader::{EvmBlockHeader, EvmBlockHeaderFromRpc};
+
+mod blockheader;
+
+fn encode_block_header(header: &EvmBlockHeader) -> Vec<u8> {
+    let mut stream = RlpStream::new();
+    header.rlp_append(&mut stream);
+    stream.out().to_vec()
+}
+
+pub async fn get_encoded_block_header() -> Result<()> {
+    let client = Provider::<Http>::try_from(
+        "https://eth-goerli.g.alchemy.com/v2/OxCXO750oi6BTN1kndUMScfn6a16gFIm",
+    )
+    .expect("could not instantiate HTTP Provider");
+
+    let client = Arc::new(client);
+
+    let block = client.get_block(BlockNumber::Latest).await?.unwrap();
+
+    println!("{:?}", block.number);
+
+    let header_from_rpc = EvmBlockHeaderFromRpc {
+        number: block.number.unwrap().as_u64().to_string(),
+        hash: hex::encode(block.hash.unwrap()),
+        difficulty: block.difficulty.to_string(),
+        extra_data: hex::encode(block.extra_data),
+        gas_limit: block.gas_limit.to_string(),
+        gas_used: block.gas_used.to_string(),
+        logs_bloom: hex::encode(block.logs_bloom.unwrap()),
+        miner: hex::encode(block.author.unwrap()),
+        mix_hash: hex::encode(block.mix_hash.unwrap()),
+        nonce: hex::encode(block.nonce.unwrap()),
+        parent_hash: hex::encode(block.parent_hash),
+        receipts_root: hex::encode(block.receipts_root),
+        sha3_uncles: hex::encode(block.uncles_hash),
+        size: block.size.unwrap().to_string(),
+        state_root: hex::encode(block.state_root),
+        timestamp: block.timestamp.to_string(),
+        total_difficulty: block.total_difficulty.unwrap().to_string(),
+        transactions_root: hex::encode(block.transactions_root),
+        base_fee_per_gas: block.base_fee_per_gas.map(|value| value.to_string()),
+        withdrawals_root: block.withdrawals_root.map(|value| hex::encode(value)),
+    };
+
+    // println!("{:#?}\n", header_from_rpc);
+
+    let evm_block_header = EvmBlockHeader::from(&header_from_rpc);
+
+    println!("{:#?}", evm_block_header);
+
+    let encoded_block_header = encode_block_header(&evm_block_header);
+    let blockheader_hex = hex::encode(&encoded_block_header);
+    println!("Hexadecimal Block Header: 0x{}", blockheader_hex);
+
+    let blockhash = keccak256(encoded_block_header);
+    let blockhash_hex = hex::encode(blockhash);
+
+    println!("Hexadecimal Block Hash: 0x{}", blockhash_hex);
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    let _ = get_encoded_block_header().await;
+}
