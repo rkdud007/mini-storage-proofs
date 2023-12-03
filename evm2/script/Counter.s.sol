8d241b49ea8a64c2d3368b582f6d2fd0903b641a
Pia <gayeongparkk@gmail.com>
2023-12-03 19:24:31 +0900

    evm2

--- evm2/script/Counter.s.sol
+++ evm2/script/Counter.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity >0.5.0 <0.8.21;
 
 import {Script, console2} from "forge-std/Script.sol";
 
--- evm2/src/EVMStorageproof.sol
+++ evm2/src/EVMStorageproof.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity >0.5.0 <0.8.21;
 
 import "../lib/Solidity-RLP/contracts/RLPReader.sol";
 import "./lib/external/trie/Lib_SecureMerkleTrie.sol";
